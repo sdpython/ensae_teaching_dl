49c64dea44a4c0e4c55eb8f3a7ca5e05a6f1c1c6
xavier dupré <xavier.dupre@gmail.com>
2018-10-06 12:31:37 +0200

    split one unit test about notebook

--- _unittests/ut_documentation/test_1_2_3_coverage_notebook6_201711_torch_convolution_mnist.py
+++ _unittests/ut_documentation/test_1_2_3_coverage_notebook6_201711_torch_convolution_mnist.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-@brief      test log(time=26s)
+@brief      test log(time=62s)
 """
 
 import sys
@@ -27,13 +27,13 @@ except ImportError:
 import src.ensae_teaching_dl
 
 
-class TestNotebook1236Coverage201711torch_mnist(ExtTestCase):
+class TestNotebook1236Coverage201711torch_convolution_mnist(ExtTestCase):
 
     def setUp(self):
         add_missing_development_version(["pymyinstall", "pyensae", "jyquickhelper"],
                                         __file__, hide=True)
 
-    def test_notebook_torch_mnist(self):
+    def test_notebook_torch_perceptron_convolution_mnist(self):
         fLOG(
             __file__,
             self._testMethodName,
@@ -42,7 +42,7 @@ class TestNotebook1236Coverage201711torch_mnist(ExtTestCase):
         self.assertTrue(src.ensae_teaching_dl is not None)
         folder = os.path.join(os.path.dirname(__file__),
                               "..", "..", "_doc", "notebooks", "101")
-        test_notebook_execution_coverage(__file__, "MNIST", folder,
+        test_notebook_execution_coverage(__file__, "210_Convolution_MNIST", folder,
                                          this_module_name="ensae_teaching_dl", fLOG=fLOG)
 
 
